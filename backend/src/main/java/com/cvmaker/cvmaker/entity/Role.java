4c07b565c5610e8885a97630824d0776b3c3f59a
Rith <mobshigeo99@gmail.com>
2025-01-18 02:46:43 +0700

    updated

--- backend/src/main/java/com/cvmaker/cvmaker/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.cvmaker.cvmaker.entity;
-
-public enum Role {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/cvmaker/cvmaker/repository/ResumeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cvmaker.cvmaker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cvmaker.cvmaker.entity.Resume;
-
-public interface ResumeRepository extends JpaRepository<Resume, Long> {
-}
